005c4e52c43da7f8e6d8451788f5b8bfa2e39650
ElCholoGamer <josedanielgrayson@gmail.com>
2020-11-28 15:19:31 -0500

    remove stringify() function

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,13 +1,3 @@
-export const stringify = (obj: any, encode = false) =>
-	Object.keys(obj)
-		.map(
-			key =>
-				`${encode ? encodeURIComponent(key) : key}=${
-					encode ? encodeURIComponent(obj[key]) : obj[key]
-				}`
-		)
-		.join('&');
-
 // Will probably add more to this in the future
 export interface User {
 	_id: string;
